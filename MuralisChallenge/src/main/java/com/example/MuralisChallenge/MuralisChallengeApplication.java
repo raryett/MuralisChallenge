e12112759b894a13719a8c074137b44b9f3ff339
raryett <raryett@outlook.com>
2025-03-20 13:37:47 -0300

    Primeiro Commit de conexao

---
+++ MuralisChallenge/src/main/java/com/example/MuralisChallenge/MuralisChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MuralisChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuralisChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuralisChallengeApplication.class, args);
+	}
+
+}
---
+++ MuralisChallenge/src/test/java/com/example/MuralisChallenge/MuralisChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MuralisChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuralisChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
